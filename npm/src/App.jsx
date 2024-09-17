d68f4abbae8285c247c38a110bb3b537b918828b
Anastasiia Sevastianova <sevasyanova.anastasia1@gmail.com>
2024-09-17 10:24:02 +0200

    install react-unicons for icons

--- npm/src/App.jsx
+++ npm/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
 
 function App() {
   
   return (
     <>
      <h1 className="text-2xl font-bold text-red-500">Hallo</h1>
+     <UilReact size={50} />
     </>
   )
 }
