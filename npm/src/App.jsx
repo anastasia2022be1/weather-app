1b5dfd245dceb01956dd37541d9e4b18c4a693ac
Anastasiia Sevastianova <sevasyanova.anastasia1@gmail.com>
2024-09-17 13:05:30 +0200

    add TemperatureAndDetails component, add icons to page

--- npm/src/App.jsx
+++ npm/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import TopButtons from './components/TopButtons.jsx'
 import Inputs from './components/Inputs.jsx'
 import TimeAndLocation from './components/TimeAndLocation.jsx'
+import TemperatureAndDetails from './components/TemperatureAndDetails.jsx'
 
 function App() {
   
@@ -10,8 +11,9 @@ function App() {
   <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400'>
     <TopButtons />
     <Inputs />
-    
+
     <TimeAndLocation />
+    <TemperatureAndDetails />
   </div>
     </>
   )
