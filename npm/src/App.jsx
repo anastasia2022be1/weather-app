30af5bc604cc0b71bca1cc75ba4329564ac4bc43
Anastasiia Sevastianova <sevasyanova.anastasia1@gmail.com>
2024-09-17 11:35:43 +0200

    add Input component, styled it with tailwinds, add icons;  add TimeAndLocation component, styles it with Tailwinds

--- npm/src/App.jsx
+++ npm/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
-import UilReact from '@iconscout/react-unicons/icons/uil-react'
 import TopButtons from './components/TopButtons.jsx'
+import Inputs from './components/Inputs.jsx'
+import TimeAndLocation from './components/TimeAndLocation.jsx'
 
 function App() {
   
@@ -8,6 +9,9 @@ function App() {
     <>
   <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400'>
     <TopButtons />
+    <Inputs />
+    
+    <TimeAndLocation />
   </div>
     </>
   )
